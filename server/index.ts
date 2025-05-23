c8d04dad114e6eb892ae5b2f1eed601fa4629222
Prashil Vaishnani <prashil.v@simformsolutions.com>
2025-05-23 15:58:01 +0530

    feat(server): implement geoip-based IP and country filtering in WAF middleware

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const app = express();
 // Basic rate limiter: 100 requests per 15 minutes per IP
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 100 requests per windowMs
+  max: 50, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 
--- server/wafMiddleware.ts
+++ server/wafMiddleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+const geoip = require("geoip-lite");
 
 const patterns: RegExp[] = [
   /<script.*?>/i,
@@ -6,7 +7,39 @@ const patterns: RegExp[] = [
   /drop\s+table/i,
   /union\s+select/i,
 ];
+const BLACKLISTED_IPS = new Set(["10.0.0.5"]);
 
+function isIpBlacklisted(ip: string) {
+  return BLACKLISTED_IPS.has(ip);
+}
+const ALLOWED_COUNTRIES = new Set(["US", "IN"]);
+
+function isCountryAllowed(ip: string) {
+  console.log(ip);
+  const geo = geoip.lookup(ip);
+  console.log(geo);
+  if (!geo) return false;
+  console.log(geo.country);
+  return ALLOWED_COUNTRIES.has(geo.country);
+}
+const lastRequestTime = {};
+
+function isBot(req: Request): boolean {
+  const userAgent = (req.headers["user-agent"] as string) || "";
+  if (!userAgent || userAgent.toLowerCase().includes("bot")) return true;
+
+  const ip = req.ip;
+  const now = Date.now();
+  if (typeof ip === "string" && ip) {
+    if (
+      (lastRequestTime as Record<string, number>)[ip] &&
+      now - (lastRequestTime as Record<string, number>)[ip] < 1000
+    )
+      return true; // 1 second
+    (lastRequestTime as Record<string, number>)[ip] = now;
+  }
+  return false;
+}
 export function wafMiddleware(
   req: Request,
   res: Response,
@@ -21,6 +54,20 @@ export function wafMiddleware(
       res.status(403).send("Forbidden: Suspicious request detected.");
       return;
     }
+  }
+  const ip = req.ip || "";
+
+  if (isIpBlacklisted(ip)) {
+    res.status(403).send("Forbidden: Blacklisted IP");
+    return;
+  }
+  if (!isCountryAllowed(ip)) {
+    res.status(403).send("Forbidden: Country not allowed");
+    return;
+  }
+  if (isBot(req)) {
+    res.status(403).send("Forbidden: Bot detected");
+    return;
   }
   next();
 }
